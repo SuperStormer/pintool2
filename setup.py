aef4823f7559c620ffc3e4256c6e5126e11ac79e
Larry Xue <larry.p.xue@gmail.com>
2021-07-10 22:02:41 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import glob
+
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+setuptools.setup(
+	name="pintool",
+	version="0.1",
+	descripton="Program for playing with Pin",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	license="MIT",
+	author="SuperStormer",
+	author_email="larry.p.xue@gmail.com",
+	url="https://github.com/SuperStormer/pintool2",
+	project_urls={"Source Code": "https://github.com/SuperStormer/pintool2"},
+	entry_points={"console_scripts": ["pintool=pintool.pintool:main"]},
+)
